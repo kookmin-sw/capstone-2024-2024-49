50243cbcc81a078ba22db23ba548121a520f6d7f
K <kms1025@kookmin.ac.kr>
2024-04-20 03:20:39 +0900

    상태관리

---
+++ lib/shared/blocs.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:luckymoon/features/board/cubit/board_cubit.dart';
+
+import '../features/chat/cubit/chat_cubit.dart';
+import '../features/home/cubit/home_cubit.dart';
+
+class BlocWidget extends StatelessWidget {
+  const BlocWidget({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<HomeCubit>(
+          create: (BuildContext context) => HomeCubit(),
+        ),
+        BlocProvider<BoardCubit>(
+          create: (BuildContext context) => BoardCubit(),
+        ),
+        BlocProvider<ChatCubit>(
+          create: (BuildContext context) => ChatCubit(),
+        ),
+      ],
+      child: child,
+    );
+  }
+}
